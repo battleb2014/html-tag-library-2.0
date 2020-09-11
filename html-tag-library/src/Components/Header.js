cfb226c439f52e4f292d6b34e8cac7f28cdbc602
Brandon <battleb2014@gmail.com>
2020-09-11 12:25:24 -0400

    setup

---
+++ html-tag-library/src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1>Complete Html Tag Library</h1>
+            <input type='text' placeholder='search'></input>
+        </div>
+    );
+}
+
+export default Header;
