9de05560ed91567291acc7fd316ae1a109d961d3
Brandon <battleb2014@gmail.com>
2020-09-11 20:23:32 -0400

    Initial structure build

--- html-tag-library/src/Components/Header.js
+++ html-tag-library/src/Components/Header.js
@@ -5,6 +5,7 @@ const Header = () => {
         <div className='header'>
             <h1>Complete Html Tag Library</h1>
             <input type='text' placeholder='search'></input>
+            <div className='magnifying-glass'></div>
         </div>
     );
 }
